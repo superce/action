6162f085f0a52653a9d1bf4eb3d0e8bdf6acc3d4
superce <562923778@qq.com>
2017-03-05 14:34:25 +0800

    ss

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import Routers from './routes'
-import './main.css';
-import 'highlight.js/styles/ocean.css'
-render(<Routers />,document.getElementById('root'));
